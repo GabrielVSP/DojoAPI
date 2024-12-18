776d9ffb8bc8c857753c658826194acae1900800
Gabriel Vergílio <125078273+GabrielVSP@users.noreply.github.com>
2024-12-18 09:24:45 -0300

    Add: PATCH and DELETE methods to Books

--- app/api/books/[bookId]/route.ts
+++ app/api/books/[bookId]/route.ts
@@ -22,3 +22,56 @@ export async function GET( req: Request, { params }: { params: {bookId: string}}
     }
 
 }
+
+export async function PATCH(req: Request, { params }: { params: {bookId: string}}) {
+
+    try {
+
+        const body = await req.json()
+        const {title, author, genre} = body
+    
+        if (!params.bookId) return new NextResponse("ID inválido", { status: 400 })
+        
+        const book = await prismadb.book.update({
+            where: {
+                id: params.bookId
+            },
+            data: {
+                title,
+                author,
+                genre
+            }
+        })
+
+        return NextResponse.json(book)
+
+    } catch (e) {
+
+        return new NextResponse("Erro interno", { status: 500})
+
+    }
+
+}
+
+export async function DELETE(req: Request, { params }: { params: {bookId: string}}) {
+
+    try {
+
+    
+        if (!params.bookId) return new NextResponse("ID inválido", { status: 400 })
+        
+        const book = await prismadb.book.delete({
+            where: {
+                id: params.bookId
+            }
+        })
+
+        return NextResponse.json(book)
+
+    } catch (e) {
+
+        return new NextResponse("Erro interno", { status: 500})
+
+    }
+
+}
